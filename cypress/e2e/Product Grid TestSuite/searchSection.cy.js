fcd33a0ae4828827129379a1d11211da4b0a9bc1
abdullah-aladham <abdullah.aladham@gmail.com>
2025-02-03 13:28:08 +0200

    added new testcases in automation testcases[Abstract].docx file

--- cypress/e2e/Product Grid TestSuite/searchSection.cy.js
+++ cypress/e2e/Product Grid TestSuite/searchSection.cy.js
@@ -1,5 +1,6 @@
 describe('Search Section Testsuite',()=>{
     it('[005-003-01] [invalid] trying to search without typing in search bar.',()=>{
-        
+        cy.get("button[type='submit']").should('be.visible').and('not.be.disabled').click()
     })
+    
 })
