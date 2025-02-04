7d300c90ea4753c3b6027ba7d17e5991c2f4cd33
abdullah-aladham <abdullah.aladham@gmail.com>
2025-02-04 19:50:35 +0200

    added Cart TestSuite

--- cypress/e2e/Product Grid TestSuite/price-range.cy.js
+++ cypress/e2e/Product Grid TestSuite/price-range.cy.js
@@ -2,21 +2,29 @@ describe('price range TestSuite',()=>{
     it('[005-002-01][Valid] Range between 1-50.',()=>{
         cy.get("span[aria-label='ngx-slider']").should('be.visible').and('not.be.disabled')
         .invoke('val','1')
+        .trigger('input');
+        cy.get("span[aria-label='ngx-slider']").should('have.value','1');
+        cy.get("span[aria-label='ngx-slider-max']").should('be.visible').and('not.be.disabled')
+        .invoke('val','50')
+        .trigger('input');
+        cy.get("span[aria-label='ngx-slider-max']").should('have.value','50');
+
+    })
+   
+    it('[005-002-02][Valid] Range between 1-50.',()=>{
+        cy.get("span[aria-label='ngx-slider']").should('be.visible').and('not.be.disabled')
+        .invoke('val','1')
         .trigger('input')
         .should('have.value','1');
         cy.get("span[aria-label='ngx-slider-max']").should('be.visible').and('not.be.disabled')
         .invoke('val','50')
-        .trigger('input');
-
-    })
-    it('[005-002-01][Valid] Range between 1-50.',()=>{
-        cy.get("span[aria-label='ngx-slider']").should('be.visible').and('not.be.disabled')
-        .invoke('val','51')
-        .trigger('input')
-        .should('have.value','51');
-        cy.get("span[aria-label='ngx-slider-max']").should('be.visible').and('not.be.disabled')
-        .invoke('val','100')
-        .trigger('input').should('have.value','51');
+        .trigger('input').should('have.value','50');
+        cy.intercept({
+            method:"GET",
+            url:'https://api.practicesoftwaretesting.com/products?between=price,1,50&page=1',
+            
+        }).as('GetPriceRange101to150');
+        cy.wait('@GetPriceRange101to150');
 
     })
     it('[005-002-03][Valid] Range between 101-150',()=>{
@@ -27,6 +35,15 @@ describe('price range TestSuite',()=>{
         cy.get("span[aria-label='ngx-slider-max']").should('be.visible').and('not.be.disabled')
         .invoke('val','150')
         .trigger('input').should('have.value','150');
+        cy.intercept({
+            method:"GET",
+            url:'https://api.practicesoftwaretesting.com/products?between=price,101,150&page=1',
+            
+        }).as('GetPriceRange101to150');
+        cy.wait('@GetPriceRange101to150');
+        cy.get("div[class='col-md-9'] div[class='container']").should('be.visible').and('not.have.text','There are no products found.');
+
+
 
     })
     it('[005-002-04][Valid] Range between 151-200',()=>{
@@ -37,6 +54,14 @@ describe('price range TestSuite',()=>{
         cy.get("span[aria-label='ngx-slider-max']").should('be.visible').and('not.be.disabled')
         .invoke('val','200')
         .trigger('input').should('have.value','200');
+        cy.intercept({
+            method:"GET",
+            url:'https://api.practicesoftwaretesting.com/products?between=price,1,100&page=1',
+            
+        }).as('GetPriceRange151to200');
+        cy.wait('@GetPriceRange151to200');
+        cy.get("div[class='col-md-9'] div[class='container']").should('be.visible').and('not.have.text','There are no products found.');
+
 
     })
     it('[005-002-05][Valid] Range between 0-0',()=>{
@@ -47,6 +72,36 @@ describe('price range TestSuite',()=>{
         cy.get("span[aria-label='ngx-slider-max']").should('be.visible').and('not.be.disabled')
         .invoke('val','0')
         .trigger('input').should('have.value','0');
+        cy.intercept({
+            method:"GET",
+            url:'https://api.practicesoftwaretesting.com/products?between=price,1,100&page=1',
+            
+        }).as('GetPriceRange0to0');
+        cy.wait('@GetPriceRange0to0');
+        cy.get("div[class='col-md-9'] div[class='container']").should('be.visible').and('have.text','There are no products found.');
+
+
+
+
+    })
+    it('[005-002-06][Valid][Default] Range between 1-100.',()=>{
+        cy.get("span[aria-label='ngx-slider']").should('be.visible').and('not.be.disabled')
+        .invoke('val','1')
+        .trigger('input');
+        cy.get("span[aria-label='ngx-slider']").should('have.value','1');
+        cy.get("span[aria-label='ngx-slider-max']").should('be.visible').and('not.be.disabled')
+        .invoke('val','100')
+        .trigger('input');
+        cy.get("span[aria-label='ngx-slider-max']").should('have.value','100');
+        cy.intercept({
+            method:"GET",
+            url:'https://api.practicesoftwaretesting.com/products?between=price,1,100&page=1',
+            
+        }).as('GetPriceRange1to100');
+        cy.wait('@GetPriceRange1to100');
+        cy.get("div[class='col-md-9'] div[class='container']").should('be.visible').and('not.have.text','There are no products found.');
+
+
 
     })
     
