1d75da1f286d06a1f70971fa372c82dd5e8ae81c
abdullah-aladham <abdullah.aladham@gmail.com>
2025-02-06 01:08:41 +0200

    added some testcases to product filters

--- cypress/e2e/Invoices-TestSuites/invoice.cy.js
+++ cypress/e2e/Invoices-TestSuites/invoice.cy.js
@@ -1,4 +1,18 @@
 describe('Invoices TestSuite',()=>{
+    function nav_Single_page(){
+        cy.get('body app-root app-invoices li:nth-child(3) > .page-link').then($button=>{
+          if($button.prop("disabled",true)){
+            
+           
+            cy.end() 
+      
+          }
+          else{
+            cy.get('body app-root app-invoices li:nth-child(3) > .page-link').should('not.be.disabled').and('be.visible').click()
+            
+          }
+        })
+      }
     it("[007-0001] [Valid] Visiting Invoices page after sign in.",()=>{
         cy.login()
         cy.get(".dropdown-item[data-test='nav-my-invoices']").should('be.visible').and('not.be.disabled').click();
@@ -61,40 +75,53 @@ describe('Invoices TestSuite',()=>{
         cy.login();
         cy.get(".dropdown-item[data-test='nav-my-invoices']").should('be.visible').and('not.be.disabled').click();
         cy.get("tbody tr:nth-child(1) td:nth-child(5) a:nth-child(1)").should('be.visible').and('not.be.disabled').click();
+       nav_pages();
         cy.intercept({
-            method:"OPTIONS",
-            url:"https://api.practicesoftwaretesting.com/users/refresh"
-        }).as("RefreshOptionsReq");
+            method:"GET",
+            url:"https://api.practicesoftwaretesting.com/invoices?page=2"
+        }).as("PageTwo");
         cy.intercept({
             method:"GET",
             url:"https://api.practicesoftwaretesting.com/users/refresh"
         }).as("RefreshReq");
-        cy.intercept({
-            method:"OPTIONS",
-            url:"https://api.practicesoftwaretesting.com/users/me"
-        }).as("me");
-        cy.intercept({
-            method:"OPTIONS",
-            url:"https://api.practicesoftwaretesting.com/invoices/01JK9K2GZBTXE6GXXX5854Z4W6"
-        }).as("InvoicesOptionsReq");
-        cy.intercept({
-            method:"GET",
-            url:"https://api.practicesoftwaretesting.com/invoices/01JK9K2GZBTXE6GXXX5854Z4W6"
-        }).as("InvoicesReq");
-        cy.intercept({
-            method:"OPTIONS",
-            url:"https://api.practicesoftwaretesting.com/invoices/INV-20240000038/download-pdf-status"
-        }).as("DownloadPdfstatusOptionsReq");
-        cy.intercept({
-            method:"GET",
-            url:"https://api.practicesoftwaretesting.com/invoices/INV-20240000038/download-pdf-status"
-        }).as("DownloadPdfstatus");
-
         
+       
     })
     	
     it('[007-0002] [Invalid] Visit invoices page without sign in',()=>{
         cy.visit("account/invoices/");
+        cy.intercept({
+            method:"GET",
+            url:"https://api.practicesoftwaretesting.com/users/me"
+        }).as("loginFailReq")
+        cy.wait("@loginFailReq").then(interception =>{
+            console.log(interception);
+            cy.wrap(interception.response.statusCode).should('eq',401)
+        })
+        cy.intercept({
+            method:"OPTIONS",
+            url:"https://api.practicesoftwaretesting.com/users/refresh"
+        }).as("RefreshOptionsReq")
+        cy.wait("@RefreshOptionsReq").then(interception =>{
+            console.log(interception);
+            cy.wrap(interception.response.statusCode).should('eq',204)
+        })
+        cy.intercept({
+            method:"GET",
+            url:"https://api.practicesoftwaretesting.com/users/refresh"
+        }).as("RefreshOptionsReq")
+        cy.wait("@RefreshOptionsReq").then(interception =>{
+            console.log(interception);
+            cy.wrap(interception.response.statusCode).should('eq',500)
+        })
+        cy.intercept({
+            method:"GET",
+            url:"https://practicesoftwaretesting.com/src_app_auth_auth_module_ts.63a6bb230c8cb86d.js"
+        }).as("Auth_ModuleGETReq")
+        cy.wait("@RefreshOptionsReq").then(interception =>{
+            console.log(interception);
+            cy.wrap(interception.response.statusCode).should('eq',200)
+        })
     })
 
 })
---
+++ cypress/e2e/Product_Filters/productFilters.cy.js
@@ -0,0 +1,169 @@
+describe('Product Filters TestSuite',()=>{
+    it('[009-0001] Filter products by Checking “hand tools” Check box',()=>{
+       cy.get("body > app-root:nth-child(2) > div:nth-child(2) > app-overview:nth-child(2) > div:nth-child(3) > div:nth-child(2) > fieldset:nth-child(13) > div:nth-child(2) > label:nth-child(1)").check(); 
+       cy.intercept({method:"GET",
+        url:"https://api.practicesoftwaretesting.com/products?between=price,1,100&by_category=01JKBGW30PCSEAWBHV1HYWNK8R,01JKBGW30T1AS7PCCD3NN4KYQS,01JKBGW30T1AS7PCCD3NN4KYQT,01JKBGW30T1AS7PCCD3NN4KYQV,01JKBGW30T1AS7PCCD3NN4KYQW,01JKBGW30T1AS7PCCD3NN4KYQX,01JKBGW30T1AS7PCCD3NN4KYQY,01JKBGW30T1AS7PCCD3NN4KYQZ&page=0"
+
+       }).as("getAllProdbyCategoryandpriceRange");
+       cy.wait("@getAllProdbyCategoryandpriceRange")
+       .then(interception =>{
+        console.log(interception)
+        cy.wrap(interception.response.statusCode).should('eq',200)
+
+       });
+       cy.intercept({method:"OPTIONS",
+        url:"https://api.practicesoftwaretesting.com/products?between=price,1,100&by_category=01JKBGW30PCSEAWBHV1HYWNK8R,01JKBGW30T1AS7PCCD3NN4KYQS,01JKBGW30T1AS7PCCD3NN4KYQT,01JKBGW30T1AS7PCCD3NN4KYQV,01JKBGW30T1AS7PCCD3NN4KYQW,01JKBGW30T1AS7PCCD3NN4KYQX,01JKBGW30T1AS7PCCD3NN4KYQY,01JKBGW30T1AS7PCCD3NN4KYQZ&page=0"
+
+       }).as("getAllProdbyCategoryandpriceRangeOptionsReq");
+       cy.wait("@getAllProdbyCategoryandpriceRangeOptionsReq")
+       .then(interception =>{
+        console.log(interception)
+        cy.wrap(interception.response.statusCode).should('eq',204)
+
+       });
+
+    })
+    
+    it('[009-0001-01]Checking “Hammer” subcategory only. With changing price range.',()=>{
+        cy.get("span[aria-label='ngx-slider']").should('be.visible').and('not.be.disabled')
+        .invoke('val','1')
+        .trigger('input')
+        .should('have.value','1');
+        cy.get("span[aria-label='ngx-slider-max']").should('be.visible').and('not.be.disabled')
+        .invoke('val','50')
+        .trigger('input').should('have.value','50');
+
+        cy.intercept({
+            method:"GET",
+            url:'https://api.practicesoftwaretesting.com/products?between=price,1,50&by_category=01JKBYKG03VG9ERDM7RJXEVPG0&page=0',
+            
+        }).as('GetHammersWithRange1-50');
+        cy.wait('@GetHammersWithRange1-50').then(interception =>{
+            console.log(interception)
+            cy.wrap(interception.response.statusCode).should('eq',204)
+    
+           });
+           cy.intercept({
+            method:"GET",
+            url:"https://api.practicesoftwaretesting.com/products?between=price,1,50&by_category=01JKBYKG03VG9ERDM7RJXEVPG0&page=0"
+           }).as("Response")
+           cy.wait("@Response").then(interception =>{
+            console.log(interception)
+            cy.wrap(interception.response.statusCode).should('eq',200)
+    
+           });
+
+
+    })
+   
+    it('[009-0001-02]Checking "Hand Saw" subcategory only.',()=>{
+        cy.get('body > app-root:nth-child(2) > div:nth-child(2) > app-overview:nth-child(2) > div:nth-child(3) > div:nth-child(2) > fieldset:nth-child(13) > div:nth-child(2) > ul:nth-child(2) > fieldset:nth-child(1) > div:nth-child(3) > label:nth-child(1)').check(); 
+        cy.intercept({
+            method:"OPTIONS",
+         url:"https://api.practicesoftwaretesting.com/products?between=price,1,100&by_category=01JKBYKG03VG9ERDM7RJXEVPG1&page=0"
+ 
+        }).as("getHandSawCategoryandpriceRangeOptionsReq");
+        cy.wait("@getHandSawCategoryandpriceRangeOptionsReq")
+        .then(interception =>{
+         console.log(interception)
+         cy.wrap(interception.response.statusCode).should('eq',204)
+ 
+        });
+        cy.intercept({method:"GET",
+         url:"https://api.practicesoftwaretesting.com/products?between=price,1,100&by_category=01JKBYKG03VG9ERDM7RJXEVPG1&page=0"
+ 
+        }).as("getAllProdbyCategoryandpriceRangeOptionsReq");
+        cy.wait("@getAllProdbyCategoryandpriceRange")
+        .then(interception =>{
+         console.log(interception)
+         cy.wrap(interception.response.statusCode).should('eq',200)
+ 
+        });
+
+        cy.intercept({method:"GET",
+            url:"https://practicesoftwaretesting.com/assets/img/products/saw01.avif"
+    
+           }).as("getAllProdbyCategoryandpriceRangeOptionsReq");
+           cy.wait("@getAllProdbyCategoryandpriceRange")
+           .then(interception =>{
+            console.log(interception)
+            cy.wrap(interception.response.statusCode).should('eq',200)
+    
+           });
+     })
+     	
+     it('[009-0001-03] Checking ”Wrench” subcategory only.',()=>{
+        cy.get('body > app-root:nth-child(2) > div:nth-child(2) > app-overview:nth-child(2) > div:nth-child(3) > div:nth-child(2) > fieldset:nth-child(13) > div:nth-child(2) > ul:nth-child(2) > fieldset:nth-child(1) > div:nth-child(4) > label:nth-child(1)')
+        .check(); 
+        cy.intercept({
+            method:"OPTIONS",
+         url:"https://api.practicesoftwaretesting.com/products?between=price,1,100&by_category=01JKBYKG03VG9ERDM7RJXEVPG2&page=0"
+ 
+        }).as("OptionsReq");
+        cy.wait("@OptionsReq")
+        .then(interception =>{
+         console.log(interception)
+         cy.wrap(interception.response.statusCode).should('eq',204)
+ 
+        });
+        cy.intercept({method:"GET",
+         url:"https://api.practicesoftwaretesting.com/products?between=price,1,100&by_category=01JKBYKG03VG9ERDM7RJXEVPG2&page=0"
+ 
+        }).as("getAllProdbyCategoryandpriceRangeOptionsReq");
+        cy.wait("@getAllProdbyCategoryandpriceRange")
+        .then(interception =>{
+         console.log(interception)
+         cy.wrap(interception.response.statusCode).should('eq',200)
+ 
+        });
+
+        cy.intercept({method:"GET",
+            url:"https://practicesoftwaretesting.com/assets/img/products/saw01.avif"
+    
+           }).as("getAllProdbyCategoryandpriceRangeOptionsReq");
+           cy.wait("@getAllProdbyCategoryandpriceRange")
+           .then(interception =>{
+            console.log(interception)
+            cy.wrap(interception.response.statusCode).should('eq',200)
+    
+           });
+     })
+     	
+     it('[009-0001-03] Checking ”Wrench” subcategory only.',()=>{
+        cy.get('body > app-root:nth-child(2) > div:nth-child(2) > app-overview:nth-child(2) > div:nth-child(3) > div:nth-child(2) > fieldset:nth-child(13) > div:nth-child(2) > ul:nth-child(2) > fieldset:nth-child(1) > div:nth-child(4) > label:nth-child(1)')
+        .check(); 
+        cy.intercept({
+            method:"OPTIONS",
+         url:"https://api.practicesoftwaretesting.com/products?between=price,1,100&by_category=01JKBYKG03VG9ERDM7RJXEVPG2&page=0"
+ 
+        }).as("OptionsReq");
+        cy.wait("@OptionsReq")
+        .then(interception =>{
+         console.log(interception)
+         cy.wrap(interception.response.statusCode).should('eq',204)
+ 
+        });
+        cy.intercept({method:"GET",
+         url:"https://api.practicesoftwaretesting.com/products?between=price,1,100&by_category=01JKBYKG03VG9ERDM7RJXEVPG2&page=0"
+ 
+        }).as("getAllProdbyCategoryandpriceRangeOptionsReq");
+        cy.wait("@getAllProdbyCategoryandpriceRange")
+        .then(interception =>{
+         console.log(interception)
+         cy.wrap(interception.response.statusCode).should('eq',200)
+ 
+        });
+
+        cy.intercept({method:"GET",
+            url:"https://practicesoftwaretesting.com/assets/img/products/saw01.avif"
+    
+           }).as("getAllProdbyCategoryandpriceRangeOptionsReq");
+           cy.wait("@getAllProdbyCategoryandpriceRange")
+           .then(interception =>{
+            console.log(interception)
+            cy.wrap(interception.response.statusCode).should('eq',200)
+    
+           });
+     })
+
+})
